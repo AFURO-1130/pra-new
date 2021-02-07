cce4f0cdc3e93f8d7c9048d19e9760cffaac8c90
AFURO-1130 <email@example.com>
2021-02-07 22:33:19 +0900

    for文

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const list =[1,2,3,4,5,6,7];
+// const list =[1,2,3,4,5,6,7];
 
-for(i=0; i<list.length; i++){
-    console.log(list[i]);
+// for(i=0; i<list.length; i++){
+//     console.log(list[i]);
+// }
+
+var array = [1,2,3,4,5];
+ 
+for( var i=0; i<array.length; i++) {
+    
+  if( array[i] == 3) break;
+  
+  console.log( array[i] );
+ 
 }
