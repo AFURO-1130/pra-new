be4235a25dcc79a788414efd70dab0507d46a851
AFURO-1130 <softtennis6161130@icloud.com>
2021-02-17 23:19:53 +0900

    api叩いてみた

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-let hoge = 2;
+const https = require('https');
+// Bitcoin のレートを json で取得することができるAPI
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&lang=ja&APPID=70231f04896404eaf5c37b7bf3bb63ee";
 
-let i =7;//ここが厳密演算子ではないから
-function my(){
-  if(hoge >0){
-    let i =0;
-    
-    console.log(i);
-  }
-}
-
-
-my();
-// count = 0;
-// function count_change(){
-//   count++;
-//   if(count < 5){
-//       count = 0;
-//   }
-//   count_show();
-// }
+https.get(URL, function (res) {
+    var body = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        body += chunk;
+    });
+    res.on('data', function (chunk) {
+        // body の値を json としてパースしている
+        res = JSON.parse(body);
+        console.log(res);
+    })
+  }).on('error', function (e) {
+    console.log(e.message);
+});
